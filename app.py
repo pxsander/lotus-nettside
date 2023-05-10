924b1bb4ae7cb2dcc49ed1b182a3815db1b0ca20
Philip Paulsen <philipa.paulsen@gmail.com>
2023-05-10 07:56:12 +0200

    update

--- app.py
+++ app.py
@@ -1,70 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for, request
 import sqlite3 as sql
-#dette er flask koden her finner du meste parten av koden til nettsiden
+
 app = Flask(__name__)
-#dette er koden som lager en linken til hver side 
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/aboutus')
-def aboutinfo():
-    return render_template('aboutus.html')
+@app.route('/enternew')
+def new_student():
+    return render_template('student.html')
 
 @app.route('/login')
-def login():
+def new_login():
     return render_template('login.html')
 
-@app.route('/signein')
-def new_student():
-    return render_template('signein.html')
-#dette er koden sheker hva du skrev i sign in og putter det inn i databasen 
+
 @app.route('/addrec', methods=['POST','GET'])
 def addrec():
     if request.method == 'POST':
         try:
             username=request.form['username']
             pwd=request.form['pwd']
-            number=request.form["number"]
-            epost=request.form["epost"]
 
             with sql.connect("database.db") as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO login (username, pwd, number, epost) VALUES (?,?,?,?)",(username,pwd,number,epost))
+                cur.execute("INSERT INTO students (username, pwd) VALUES (?,?)",(username,pwd))
                 con.commit()
                 msg = "Record sucessfully added"
-        except Exception as e:
+        except:
             con.rollback()
-            msg="error in insert operation" + str(e)
+            msg="error in insert operation"
         finally:
-            con.close()
             return render_template("result.html", msg=msg)
+            con.close()
 
-#dette er koden som sheker login informashonen din og gir deg riktig svar and på hva du skriver
-@app.route('/testrec', methods=['POST'])
+@app.route('/testrec', methods=['POST','GET'])
 def testrec():
-    try:
-        username=request.form['username'] 
-        pwd=request.form['pwd']
-        with sql.connect("database.db") as con:
-            cur = con.cursor()
-        
-            sqlite_insert_query = """select * from login where username='""" + username + """' and pwd='""" + pwd + """'"""
-            cur.execute(sqlite_insert_query)
-            records = cur.fetchall()
-    except:
-        msg="wrong loging informayshoin"
-    finally:
-        con.close()
-        if (len(records) >= 1):
-            msg = "login sucsefull" + " " + str(username)
-            return render_template("loginres.html", msg=msg)
-        else:
-            msg = "wrong loging informayshoi"
-            return render_template("login.html", msg=msg)
-            
+    if request.method == 'POST':
+        try:
+            username=request.form['username']
+            pwd=request.form['pwd']
 
-#dette er koden som tar informashon fra databasen og sender den til tabelen i lite.html
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+            
+                sqlite_insert_query = """select * from login where username='""" + username + """' and pwd='""" + pwd + """'"""
+                cur.execute(sqlite_insert_query)
+                records = cur.fetchall()
+        except:
+            msg="wrong loging informayshoin"
+        finally:
+            if (len(records) >= 1):
+                msg = "login sucsefull" + " " + str(records)
+                return render_template("login2.html", msg=msg)
+            else:
+                msg = "wrong loging informayshoi"
+                return render_template("index.html", msg=msg)
+            con.close()
+
 @app.route('/list')
 def list():
     con = sql.connect("database.db")
